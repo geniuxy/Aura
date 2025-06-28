d5382a3ffe64e956df12a0d1e97798b5a24dd935
geniuxy <2238384910@qq.com>
2025-06-28 19:28:57 +0800

    Post Process Highlight

--- Source/Aura/Public/Character/AuraEnemy.h
+++ Source/Aura/Public/Character/AuraEnemy.h
@@ -13,6 +13,8 @@ class AURA_API AAuraEnemy : public AAuraCharacterBase, public IEnemyInterface
 	GENERATED_BODY()
 
 public:
+	AAuraEnemy();
+	
 	virtual void HighlightActor() override;
 	virtual void UnHighlightActor() override;
 
