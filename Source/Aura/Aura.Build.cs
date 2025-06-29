6ab415919f72c3eda22f99c2ffc414e2b0fbcc0f
geniuxy <2238384910@qq.com>
2025-06-30 02:11:20 +0800

    ASC and AttributeSet

--- Source/Aura/Aura.Build.cs
+++ Source/Aura/Aura.Build.cs
@@ -17,7 +17,12 @@ public class Aura : ModuleRules
 			"EnhancedInput"
 		});
 
-		PrivateDependencyModuleNames.AddRange(new string[] { });
+		PrivateDependencyModuleNames.AddRange(new string[]
+		{
+			"GameplayAbilities",
+			"GameplayTasks",
+			"GameplayTags"
+		});
 
 		// Uncomment if you are using Slate UI
 		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
