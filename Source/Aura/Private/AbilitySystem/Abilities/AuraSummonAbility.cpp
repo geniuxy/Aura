db39a170ff851b0bb492b1c78509733fdf693c83
geniuxy <2238384910@qq.com>
2025-09-10 21:39:22 +0800

    保留summon的debug代码

--- Source/Aura/Private/AbilitySystem/Abilities/AuraSummonAbility.cpp
+++ Source/Aura/Private/AbilitySystem/Abilities/AuraSummonAbility.cpp
@@ -3,6 +3,8 @@
 
 #include "AbilitySystem/Abilities/AuraSummonAbility.h"
 
+#include "Kismet/KismetSystemLibrary.h"
+
 TArray<FVector> UAuraSummonAbility::GetSpawnLocations()
 {
 	const FVector Forward = GetAvatarActorFromActorInfo()->GetActorForwardVector();
@@ -23,6 +25,11 @@ TArray<FVector> UAuraSummonAbility::GetSpawnLocations()
 			ChosenSpawnLocation = Hit.ImpactPoint;
 		}
 		SpawnLocations.Add(ChosenSpawnLocation);
+
+		// DrawDebugSphere(GetWorld(), ChosenSpawnLocation, 18.f, 12, FColor::Cyan, false, 3.f );
+		// UKismetSystemLibrary::DrawDebugArrow(GetAvatarActorFromActorInfo(), Location, Location + Direction * MaxSpawnDistance, 4.f, FLinearColor::Green, 3.f );
+		// DrawDebugSphere(GetWorld(), Location + Direction * MinSpawnDistance, 5.f, 12, FColor::Red, false, 3.f );
+		// DrawDebugSphere(GetWorld(), Location + Direction * MaxSpawnDistance, 5.f, 12, FColor::Red, false, 3.f );
 	}
 
 	return SpawnLocations;
