05b0b5746060a0204056257e814a506b359cab68
geniuxy <2238384910@qq.com>
2025-11-05 22:21:01 +0800

    Magic Circles

---
+++ Source/Aura/Private/Actor/MagicCircle.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Actor/MagicCircle.h"
+
+#include "Components/DecalComponent.h"
+
+AMagicCircle::AMagicCircle()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+	MagicCircleDecal = CreateDefaultSubobject<UDecalComponent>("MagicCircleDecal");
+	MagicCircleDecal->SetupAttachment(GetRootComponent());
+}
+
+void AMagicCircle::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+void AMagicCircle::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
