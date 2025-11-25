0a47a95167a4d9bb2ad96a465836222ae006cd54
geniuxy <2238384910@qq.com>
2025-11-25 21:54:09 +0800

    Data Structures for Saving Data

--- Source/Aura/Public/Game/LoadScreenSaveGame.h
+++ Source/Aura/Public/Game/LoadScreenSaveGame.h
@@ -46,6 +46,39 @@ inline bool operator==(const FSavedAbility& Left, const FSavedAbility& Right)
 	return Left.AbilityTag.MatchesTagExact(Right.AbilityTag);
 }
 
+USTRUCT()
+struct FSavedActor
+{
+	GENERATED_BODY()
+
+	UPROPERTY()
+	FName ActorName = FName();
+
+	UPROPERTY()
+	FTransform Transform = FTransform();
+
+	// Serialized variables from the Actor - only those marked with SaveGame specifier
+	UPROPERTY()
+	TArray<uint8> Bytes;
+};
+
+inline bool operator==(const FSavedActor& Left, const FSavedActor& Right)
+{
+	return Left.ActorName == Right.ActorName;
+}
+
+USTRUCT()
+struct FSavedMap
+{
+	GENERATED_BODY()
+
+	UPROPERTY()
+	FString MapAssetName = FString();
+
+	UPROPERTY()
+	TArray<FSavedActor> SavedActors;
+};
+
 /**
  * 
  */
@@ -107,4 +140,12 @@ public:
 
 	UPROPERTY()
 	TArray<FSavedAbility> SavedAbilities;
+
+	/* World State */
+
+	UPROPERTY()
+	TArray<FSavedMap> SavedMaps;
+
+	FSavedMap GetSavedMapWithMapName(const FString& InMapName);
+	bool HasMap(const FString& InMapName);
 };
